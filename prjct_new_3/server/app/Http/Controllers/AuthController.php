aa3384eb51f15cf0582f539cf5a12285db8c6afa
rez-w-laban <man43797@gmail.com>
2024-05-07 18:55:40 +0300

    Update AuthController.php

--- prjct_new_3/server/app/Http/Controllers/AuthController.php
+++ prjct_new_3/server/app/Http/Controllers/AuthController.php
@@ -46,17 +46,43 @@ class AuthController extends Controller
         }
     }
 
-
-    public function register(Request $request)
+public function register(Request $request)
     {
-        try {
-            $request->validate([
-                'name' => 'required|string|max:255',
-                'email' => 'required|string|email|max:255',
-                'password' => 'required|string|min:6',
-                'profile_picture' => 'nullable|mimes:jpeg,jpg,png|max:2048',
-            ]);
+       try {
+        //     $request->validate([
+        //         'name' => 'required|string|max:255',
+        //         'email' => 'required|string|email|max:255',
+        //         'password' => 'required|string|min:6',
+        //         'profile_picture' => 'nullable|mimes:jpeg,jpg,png|max:2048',
+        //     ]);
+        
+        $messages = [
+                    'required' => 'Please fill all fields',
+                    'mimes' => 'The uploaded file is not a supported format.',
+                    'max' => 'The file size exceeds the maximum limit of 2 mb.',
+                    'min'=>'password must be at least 6 characters',
+                ];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+            'profile_picture' => 'nullable|mimes:jpeg,jpg,png|max:2048',]
+            , $messages);
+
             
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 500);
+        }
+
+        
+        $existingUser = User::where('email', $request->email)->first();
+        if ($existingUser) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Email already in use',
+            ], 422);
+        }
       
 
             $user = User::create([
@@ -66,6 +92,7 @@ class AuthController extends Controller
                 'role_id' => 2 ,
                 //'profile_picture' => $request->file('profile_picture'),
             ]);
+
             if($request->profile_picture){
             $profile_picture = $request->file('profile_picture');
             $file_name = uniqid('media_') . '.' . $profile_picture->getClientOriginalExtension();
@@ -78,10 +105,10 @@ class AuthController extends Controller
         
         
             $path = Storage::putFileAs($directory, $profile_picture, $file_name);
-            $full_path="C:/xampp/htdocs/prjct_new_3/server/storage/app/$directory/$file_name";
+            //$full_path="C:/xampp/htdocs/prjct_new_3/server/storage/app/$directory/$file_name";
 
             $user->update([
-                'profile_picture'=>$full_path,
+                'profile_picture'=>$file_name,
             ]);
 
             return response()->json([
@@ -100,13 +127,6 @@ class AuthController extends Controller
             
         } catch (Exception $e) {
 
-            $existingUser = User::where('email', $request->email)->first();
-            if ($existingUser) {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Email already in use',
-                ], 422);
-            }
             return response()->json([
                 'status' => 'error',
                 'message' => 'Registration failed, try again later',
@@ -114,6 +134,7 @@ class AuthController extends Controller
         }
     }
 
+    
 
 
     public function logout()
@@ -131,4 +152,75 @@ class AuthController extends Controller
             ], 500);
         }
     }
+
+    
+    // public function  updateProfile(Request $request)
+    // {
+    //     $user = Auth::user();
+    //     $user_get = User::where('email',$user->email)->get();
+    //     $messages = [
+    //         'required' => 'Please fill all fields',
+    //         'mimes' => 'The uploaded file is not a supported format.',
+    //         'max' => 'The file size exceeds the maximum limit of 2 mb.',
+    //     ];
+    
+    //     $validator = Validator::make($request->all(), [
+    //         'name' => 'nullable|string|max:255',
+    //         'email' => 'required|string|email|max:255',
+    //         'profile_picture' => 'nullable|mimes:jpeg,jpg,png|max:2048',]
+    //         , $messages);
+            
+    //     if ($validator->fails()) {
+    //         return response()->json($validator->errors(), 400);
+    //     }
+
+    //     $existing_email = User::where('email',$request->email)->first();
+    //     if($existing_email){
+    //         return response()->json([
+    //            "status"=>"failed",
+    //            "message"=>"email already exists",
+
+    //         ]);
+    //     }
+    //     $user_get->update([
+    //         'email'=>$request->email,
+            
+    //     ]);
+
+    //     if($request->name && $request->name !== $user_get->name ){
+    //         $user_get->update([
+    //             'name'=>$request->name,
+                
+    //         ]);
+    //     }
+        
+
+
+
+    // }
+
+
+public function getMyProfile(Request $request){
+try{
+$user = Auth::user();
+
+return response()->json([
+ "status" => 'success',
+    "id"=>$user->id,
+    "name"=>$user->name,
+    "email"=>$user->email,
+    "profile_photo"=>$user->profile_photo,
+
+
+]);
+}catch(Exception $ex){
+    return response()->json([
+        "status" => 'error',
+        "message" => "failed to return your profile ; exception : $ex",
+    ]);
+
+
+}
+}
+
 }
